868b6b8256d4ca8e58f19bed3d120d3ed6401953
Tyjoo26 <Tyjoo21@gmail.com>
2018-01-04 10:05:09 -0700

    final commit

--- violations.rb
+++ violations.rb
@@ -1,13 +1,38 @@
 require 'CSV'
 require 'pry'
 
-CSV.foreach('Violations-2012.csv') do |row|
-  @violations = []
-  @violations << row
-end
+  violation = Violation.new
+  
+  options = {headers: true, header_converters: :symbol, converters: :numeric}
 
-@violations.each do |violation|
-  @type = Hash.new(0)
-  @type[violation[-1]] += 1
-  p @type.inspect
-end
+  CSV.foreach('Violations-2012.csv', options) do |row|
+    @violations = []
+    @violations << Violation.create(
+      violation_id: row[:violation_id],
+      inspection_id: row[:inspection_id],
+      violation_category: row[:violation_category],
+      violation_date: row[:violation_date],
+      violation_date_closed: row[:violation_date_closed],
+      violation_type: row[:violation_type])
+  end
+
+  p @violations
+
+
+
+
+  # def count_of_violations
+  #   @violations.each do |violation|
+  #     @type = Hash.new(0)
+  #     @type[violation[-1]] += 1
+  #   end
+  #
+  # end
+
+  # def earliest_violation_date
+  #   @violations.each do |violation|
+  #     @date =
+  # end
+
+
+# p count_of_violations
